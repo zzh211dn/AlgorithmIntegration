36299ab30bd033eb2b57778cfa1f2186c6e69581
zzh211dn <zzh211dn@163.com>
2016-12-07 15:46:32 +0800

    主界面框架

--- src/frame/MianFrame.java
+++ src/frame/MianFrame.java
@@ -3,41 +3,64 @@ package frame;
  * Created by zzh on 2016/12/7.
  */
 
+
+import java.awt.*;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
+
+import com.smooth.gui.SmoothGUI;
 import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Effect;
 import javafx.scene.effect.Glow;
 import javafx.scene.effect.SepiaTone;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.*;
+
+import static javax.swing.SwingUtilities.updateComponentTreeUI;
 
 public class MianFrame extends Application {
 
     public static void main(String[] args) {
+        System.setProperty( "javafx.userAgentStylesheetUrl", "CASPIAN" );
         launch(args);
     }
 
     @Override
     public void start(Stage stage) {
+
         stage.setTitle("算法软件");
+        final Stage primaryStage = new Stage();
         Scene scene = new Scene(new VBox(), 800, 600);
 
+        final SwingNode swingNode = new SwingNode();
+        StackPane pane = new StackPane();
         MenuBar menuBar = new MenuBar();
 
         // --- Menu File
         Menu menuFile = new Menu("文件");
 
         // --- Menu Edit
-        Menu menuEdit = new Menu("数据预处理");
+        Menu menuPre = new Menu("预处理");
 
-        // --- Menu View
-        Menu menuView = new Menu("分类算法");
+        // --- Menu analyze
+        Menu menuAnal = new Menu("分析算法");
 
         // --- Menu Classification
         Menu menuClassif = new Menu("分类算法");
@@ -45,7 +68,49 @@ public class MianFrame extends Application {
         // --- Menu Regression
         Menu menuRegression = new Menu("回归算法");
 
-        menuBar.getMenus().addAll(menuFile, menuEdit, menuView, menuClassif,menuRegression);
+        // --- Menu Maping
+        Menu menuMaping = new Menu("Maping");
+
+        //添加数据预处理菜单
+        MenuItem menuItemPre = new MenuItem("数据预处理");
+        menuPre.getItems().addAll(menuItemPre);
+
+        //添加文件子菜单
+        MenuItem addTrainSet = new MenuItem("打开训练集");
+        MenuItem addTestSet = new MenuItem("打开测试集");
+        MenuItem saveTrainSet = new MenuItem("保存训练集");
+        MenuItem saveTestSet = new MenuItem("保存测试集");
+
+        menuFile.getItems().addAll(addTrainSet,addTestSet,saveTrainSet,saveTestSet);
+
+        //添加分析算法子菜单
+        MenuItem addPLS = new MenuItem("偏最小二乘");
+        MenuItem addPCA = new MenuItem("主成分分析");
+        menuAnal.getItems().addAll(addPCA,addPLS);
+
+        //添加分类算法子菜单
+        MenuItem addSVM = new MenuItem("支持向量机");
+        MenuItem addKNN = new MenuItem("最近邻");
+        MenuItem addHCA = new MenuItem("层次聚类");
+        menuClassif.getItems().addAll(addSVM,addKNN,addHCA);
+
+        //添加回归算法子菜单
+        MenuItem addBPNN = new MenuItem("人工神经网络");
+        menuRegression.getItems().addAll(addBPNN);
+
+        //添加Maping子菜单
+        MenuItem addPrintPic = new MenuItem("画图");
+        MenuItem addKmeans = new MenuItem("K-聚类");
+        menuMaping.getItems().addAll(addPrintPic,addKmeans);
+
+        //添加各菜单到菜单栏
+
+        menuBar.getMenus().addAll(menuFile,menuPre,menuAnal,menuClassif,menuRegression,menuMaping);
+
+        //Action事件监听
+        menuItemPre.setOnAction((ActionEvent t) -> {
+            new SmoothGUI();
+        });
 
 
         ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
@@ -55,16 +120,7 @@ public class MianFrame extends Application {
     }
 
 
-    private class PageData {
-        public String name;
-        public String description;
-        public String binNames;
-        public Image image;
-        public PageData(String name, String description, String binNames) {
-            this.name = name;
-            this.description = description;
-            this.binNames = binNames;
 
-        }
-    }
+
 }
+
