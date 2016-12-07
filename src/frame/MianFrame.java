04addf66027d3a7215f61ad7f963f638cb27743f
zzh211dn <zzh211dn@163.com>
2016-12-07 13:12:09 +0800

    添加lib

--- src/frame/MianFrame.java
+++ src/frame/MianFrame.java
@@ -3,7 +3,18 @@ package frame;
  * Created by zzh on 2016/12/7.
  */
 
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Effect;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MianFrame extends Application {
@@ -13,7 +24,41 @@ public class MianFrame extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
+        stage.setTitle("算法软件");
+        Scene scene = new Scene(new VBox(), 400, 350);
 
+        MenuBar menuBar = new MenuBar();
+
+        // --- Menu File
+        Menu menuFile = new Menu("File");
+
+        // --- Menu Edit
+        Menu menuEdit = new Menu("Edit");
+
+        // --- Menu View
+        Menu menuView = new Menu("View");
+
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+
+
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    private class PageData {
+        public String name;
+        public String description;
+        public String binNames;
+        public Image image;
+        public PageData(String name, String description, String binNames) {
+            this.name = name;
+            this.description = description;
+            this.binNames = binNames;
+
+        }
     }
 }
