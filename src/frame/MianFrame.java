228eda1888274814051db381bbb61586ecb3e674
zzh211dn <zzh211dn@163.com>
2016-12-07 12:09:50 +0800

    init

---
+++ src/frame/MianFrame.java
@@ -0,0 +1,18 @@
+package frame; /**
+ * Created by zzh on 2016/12/7.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MianFrame extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
